587c1f8a4d5aa87fb36159cb5d883ce36f878a2b
Mikkel Ridley <mikkel250@gmail.com>
2018-09-09 23:54:48 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,11 @@
 def hello_t(arr)
-  i = 0
-  while i < arr.length
-    yield arr[i]
-    i = i + 1
-  end
-  
+  if block_given?
+    i = 0
+    while i < arr.length
+      yield arr[i]
+      i = i + 1
+    end
+  arr
 end
 
 # call your method here!
